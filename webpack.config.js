c91cc5f8844a62949161ce2e25de3a52d808f81c
thomasjodt <jodt5200@gmail.com>
2022-04-22 16:44:52 -0500

    Added the configuration for production mode

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const CopyPlugin = require('copy-webpack-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const DotEnv = require('dotenv-webpack')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -68,6 +69,7 @@ module.exports = {
             }
         ),
         new DotEnv(),
+        new CleanWebpackPlugin(),
     ],
     optimization: {
         minimize: true,
