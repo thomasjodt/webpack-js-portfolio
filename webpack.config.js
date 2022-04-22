bd3df084b6900dd7fb16dd0f19541f45a8e8dbfb
thomasjodt <jodt5200@gmail.com>
2022-04-22 14:41:23 -0500

    Added hashes to the elements of the project

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
+        filename: '[name].[contenthash].js',
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     resolve: {
@@ -34,7 +36,7 @@ module.exports = {
                 test: /\.(woff|woff2)$/i,
                 type: 'asset/resource',
                 generator: {
-                    filename: 'assets/fonts/[hash][ext][query]',
+                    filename: 'assets/fonts/[name].[hash][ext][query]',
                 }
             },
         ]
@@ -45,7 +47,9 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html',
         }),
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
         new CopyPlugin(
             {
                 patterns: [
@@ -57,4 +61,11 @@ module.exports = {
             }
         ),
     ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    }
 }
